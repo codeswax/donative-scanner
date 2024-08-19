6badb2b2103606ba8fc38a738b2dff544f1c8a53
Stefano Suarez Herrera <stesherr@espol.edu.ec>
2024-08-18 19:50:47 -0500

    Se agrego categoriesRoute y campaignRoute

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,15 @@ app.use(express.json());
 const donativeRoute = require("./routes/donativeRoute");
 const reportsRoute = require("./routes/reportsRoute");
 const campaignRoute = require('./routes/campaignRoute');
+const productsRoute = require("./routes/productsRoute");
+const categoriesRoute = require("./routes/categoriesRoute");
 
 // routes used
 app.use("/donative", donativeRoute);
 app.use("/report", reportsRoute);
 app.use('/campaign', campaignRoute);
+app.use('/products', productsRoute);
+app.use('/categories', categoriesRoute);
 
 app.get("/", (req, res)=>{
     res.send("Ejemplo");
